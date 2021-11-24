4231723aa8d26790002db4ee4665d35ae9a08b3b
Shurumela com estilo <pedro.maria@bandtec.com.br>
2021-11-23 21:05:45 -0300

    Site

---
+++ Opus/src/models/medidaModel.js
@@ -0,0 +1,36 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(idAquario, limite_linhas) {
+    instrucaoSql = `
+        select count(nome) from usuario group by nome;
+    `;
+
+    /* select 
+                        temperatura, 
+                        umidade, 
+                        momento,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc limit ${limite_linhas} */;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(idAquario) {
+    instrucaoSql = `select 
+                        temperatura, 
+                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc limit 1`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
